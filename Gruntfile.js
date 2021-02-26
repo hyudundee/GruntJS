9d75c482f38bc5663f1a137f46867434ba0eb06e
haoranyu <yu.haora@northeastern.edu>
2021-02-25 20:36:18 -0500

    task registration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,21 @@
 module.exports = function(grunt) {
   // configuration
   grunt.initConfig({
-    
-  })
+    // pass in options to plugins, reference to files etc
+
+  });
+  
+  // load plugins
+  // grunt.loadNpmTasks('')
+
+  // Register tasks
+  grunt.registerTask('run', () => {
+    console.log('I am running');
+  });
+
+  grunt.registerTask('sleep', () => {
+    console.log('I am sleeping');
+  });
+
+  grunt.registerTask('all', ['sleep', 'run']);
 }
